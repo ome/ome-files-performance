5e4c6d3d76eda1bf14a18a3f563201695cc48465
Roger Leigh <rleigh@dundee.ac.uk>
2017-02-14 14:54:18 +0000

    R: Add three figures

--- plots.R
+++ plots.R
@@ -4,13 +4,13 @@ library(ggplot2)
 dataset.name <- function(filename) {
     t <- "Unknown"
     if(filename=="NIRHTa-001.ome.tiff") {
-        t <- paste("BBBC (", filename, ")", sep="")
+        t <- "BBBC"
     }
     if(filename=="00001_01.ome.tiff") {
-        t <- paste("MitoCheck (", filename, ")", sep="")
+        t <- "MitoCheck"
     }
     if(filename=="tubhiswt_C0_TP0.ome.tif") {
-        t <- paste("tubhiswt (", filename, ")", sep="")
+        t <- "tubhiswt"
     }
     t
 }
@@ -41,6 +41,7 @@ read.dataset <- function(datanames, testname, includejace) {
         }
     }
 
+    df$test.name <- gsub(paste(testname, ".", sep=""), "", df$test.name)
     df$dataset <- sapply(df$test.file, dataset.name)
 
     df$Language <- factor(df$test.lang)
@@ -70,7 +71,57 @@ realtime.compare <- function(datanames, testname, includejace) {
     plot.dataset(df, testname, includejace)
 }
 
+plot.figure1 <- function() {
+    df <- read.dataset(c("bbbc", "mitocheck", "tubhiswt"), "metadata", TRUE)
+
+    filename <- "cpp-fig1.pdf"
+    cat("Creating ", filename, "\n")
+    p <- ggplot(aes(y = proc.real, x = Test, colour=Implementation), data = df) +
+      ylab("Execution time (ms)") + labs(title="Figure 1: Metadata performance") +
+      geom_boxplot(lwd=0.25, fatten = 2, outlier.size=0.5) +
+      facet_wrap(~ Dataset, scales = "free_y")
+    ggsave(filename=filename,
+           plot=p, width=6, height=3)
+}
+
+plot.figure2 <- function() {
+    df <- read.dataset(c("bbbc", "mitocheck", "tubhiswt"), "pixeldata", TRUE)
+    df <- subset(df, test.name == 'read.pixels' | test.name == 'write.pixels')
+    df$test.name <- gsub(".pixels", "", df$test.name)
+    df$Test <- factor(df$test.name)
+
+    filename <- "cpp-fig2.pdf"
+    cat("Creating ", filename, "\n")
+    p <- ggplot(aes(y = proc.real, x = Test, colour=Implementation), data = df) +
+      ylab("Execution time (ms)") + labs(title="Figure 2: Pixel data performance") +
+      geom_boxplot(lwd=0.25, fatten = 2, outlier.size=0.5) +
+      facet_wrap(~ Dataset, scales = "free_y")
+    ggsave(filename=filename,
+           plot=p, width=6, height=3)
+}
+
+plot.figure3 <- function() {
+    df <- read.dataset(c("bbbc", "mitocheck", "tubhiswt"), "pixeldata", TRUE)
+    df <- subset(df, test.name == 'read' | test.name == 'write')
+    df$test.name <- gsub("$", "er", df$test.name)
+    df$Test <- factor(df$test.name)
+
+    filename <- "cpp-fig3.pdf"
+    cat("Creating ", filename, "\n")
+    p <- ggplot(aes(y = proc.real, x = Test, colour=Implementation), data = df) +
+      ylab("Execution time (ms)") + labs(title="Figure 3: Reader and writer aggregate performance") +
+      geom_boxplot(lwd=0.25, fatten = 2, outlier.size=0.5) +
+      facet_wrap(~ Dataset, scales = "free_y")
+    ggsave(filename=filename,
+           plot=p, width=6, height=3)
+}
+
+
 realtime.compare(c("bbbc", "mitocheck", "tubhiswt"), "metadata", FALSE)
 realtime.compare(c("bbbc", "mitocheck", "tubhiswt"), "metadata", TRUE)
 realtime.compare(c("bbbc", "mitocheck", "tubhiswt"), "pixeldata", FALSE)
 realtime.compare(c("bbbc", "mitocheck", "tubhiswt"), "pixeldata", TRUE)
+
+plot.figure1()
+plot.figure2()
+plot.figure3()
