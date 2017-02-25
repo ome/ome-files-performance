37814c2cdb48b2d57e774c86796851c8e10cfe4b
Roger Leigh <rleigh@dundee.ac.uk>
2017-02-25 18:04:40 +0000

    scripts: Add  eqivalent for batch files

--- scripts/ometiff-performance-java.bat
+++ scripts/ometiff-performance-java.bat
@@ -1,3 +1,11 @@
 @echo off
 
-java -classpath "target/classes;target/dependency/*" ome.files.performance.OMETIFFPerformance "$@"
+set params=%1
+:argloop
+shift
+if [%1]==[] goto afterargloop
+set params=%params% %1
+goto argloop
+:afterargloop
+
+java -classpath "target/classes;target/dependency/*" ome.files.performance.OMETIFFPerformance %params%
